8ba07aa4a9028cffcc71a05794fcca7a8de3d646
rabierre <rabierre@gmail.com>
2013-01-28 03:03:29 +0900

    minor refactor; rename

--- paradigm/word_counter.rb
+++ paradigm/word_counter.rb
@@ -1,8 +1,8 @@
 class WordCounter
-  attr_accessor :wordCounts
+  attr_accessor :counts
 
   def initialize
-    @wordCounts = Hash.new
+    @counts = Hash.new
   end
 
   def count file_name
@@ -10,18 +10,18 @@ class WordCounter
       split_line = line.split(Regexp.compile "[\s|\t|\n]")
 
       split_line.each { |word|
-        if @wordCounts.has_key? word then
-          @wordCounts[word] += 1
+        if @counts.has_key? word then
+          @counts[word] += 1
         else
-          @wordCounts = @wordCounts.merge({word => 1})
+          @counts = @counts.merge({word => 1})
         end
       }
     }
-    @wordCounts
+    @counts
   end
 
   def printAll
-    @wordCounts.each { |wordValuePair|
+    @counts.each { |wordValuePair|
       print "key : ", wordValuePair[0], ", count : ", wordValuePair[1], "\n"
     }
   end
